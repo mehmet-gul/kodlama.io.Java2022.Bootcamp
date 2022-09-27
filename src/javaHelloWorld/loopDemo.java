aeebdea20bfa27128aa9c15086684bd0f752cefc
Mehmet GUL <mehmetgul14@gmail.com>
2022-09-27 19:37:45 +0300

    First Commit

---
+++ src/javaHelloWorld/loopDemo.java
@@ -0,0 +1,20 @@
+package javaHelloWorld;
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+		//For
+		for(int i=1;i<=5;i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Döngü Bitti");
+		
+		for(int i=1;i<=10;i=i+2) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Döngü Bitti");
+	}
+
+}
